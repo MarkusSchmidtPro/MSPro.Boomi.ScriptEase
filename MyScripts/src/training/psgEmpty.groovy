8745c68ac0a5e37d1ceb1baf6486cfc66091f108
MSPro.NBBoomi <Markus@MarkusSchmidt.Pro>
2024-11-15 12:28:13 +0100

    Update to 1.0.0

---
+++ MyScripts/src/training/psgEmpty.groovy
@@ -0,0 +1,14 @@
+package training
+
+/*  An empty Groovy Process Script, 
+    that does nothing but passing though all documents.
+    
+    NOTE: The Groovy Script language does not require semicolon at the end of each statement,
+    and to avoid unexpected situations, we should really not use it.
+ */
+for(int i = 0; i < dataContext.getDataCount(); i++ ) {
+    InputStream is = dataContext.getStream(i);
+    Properties props = dataContext.getProperties(i);
+
+    dataContext.storeStream(is, props);
+}
